33a41b649cf4c1174d05222b98763865b383b0bf
Santosh K <santosh07bec@gmail.com>
2023-03-25 17:47:02 +0530

    Updated eiifccuhelfeetdntvtketthglgtkuggknbbugttjdrc

--- GadgetsOnline/Controllers/HomeController.cs
+++ GadgetsOnline/Controllers/HomeController.cs
@@ -14,7 +14,34 @@ namespace GadgetsOnline.Controllers
         {
             inventory = new Inventory();
             var products = inventory.GetBestSellers(6);
+            ViewBag.Hostname = "; Node: " + Environment.MachineName;
+            ViewBag.AvailabilityZone = "; AZ: " + GetMetadataValue("placement/availability-zone");
+            ViewBag.InstanceType = "; Instance-Type: " + GetMetadataValue("instance-type");
             return View(products);
+        }
+
+        private static readonly Dictionary<string, string> metadataCache = new Dictionary<string, string>();
+
+        private static string GetMetadataValue(string path)
+        {
+            if (!metadataCache.ContainsKey(path))
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var response = httpClient.GetAsync($"http://169.254.169.254/latest/meta-data/{path}").Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var metadataValue = response.Content.ReadAsStringAsync().Result;
+                        metadataCache[path] = metadataValue;
+                    }
+                    else
+                    {
+                        metadataCache[path] = "Unknown";
+                    }
+                }
+            }
+
+            return metadataCache[path];
         }
 
         public ActionResult About()
